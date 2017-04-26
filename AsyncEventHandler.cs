06c0a853718e72ee261cc2df6780ec8b95d5a7d7
White <WhiteXZ@users.noreply.github.com>
2017-04-26 14:18:42 +0930

    Added an AsyncEventHandler delegate

---
+++ AsyncEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace BotApi
+{
+	/// <summary>
+	/// Allows asynchronous event handling
+	/// </summary>
+	/// <typeparam name="TArgs"></typeparam>
+	/// <param name="sender"></param>
+	/// <param name="args"></param>
+	/// <returns></returns>
+	public delegate Task AsyncEventHandler<TArgs>(object sender, TArgs args);
+}
